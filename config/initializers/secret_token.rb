dd76c72f444a3f55d2c308083fc3be0d0ce6b4a4
Grycek <adam.grycner@wp.pl>
2010-12-21 14:06:54 +0100

    Remove redundancy

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Comments::Application.config.secret_token = '7368f25229206d93f96f8213ed2398604020868d008e843d717306c9b588f9ac2accfb8746bd8c4ac071126baf5cb252d2ad7e10d3c2caab6b9d42bfcefde909'
